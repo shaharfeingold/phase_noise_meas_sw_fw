33635af623908b5734da66e6bd5cf25b0b58ae44
shaharfeingold <110739184+shaharfeingold@users.noreply.github.com>
2023-09-08 15:58:43 +0300

    fix test file debug prints

--- fw/src/test_file.c
+++ fw/src/test_file.c
@@ -7,9 +7,10 @@
 #include "utils_function.h"
 
 int main(){
+    uint64_t address = 0x42000000;
     verb_print(MED, "starting simulation\n");
-    verb_print(MED, "trying to read memory 0x42000000\n");
-    uint32_t read_data = read_from_logic(0x42000000);
+    verb_print(MED, "trying to read memory %x\n",address);
+    uint32_t read_data = read_from_logic(address);
     verb_print(MED, "read data = %d\n", read_data);
     verb_print(MED, "finish simulation, leaving now\n");
 
