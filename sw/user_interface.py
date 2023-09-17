47c0e8189c08590821fe16b595f7b77a79d8908f
shaharfeingold <110739184+shaharfeingold@users.noreply.github.com>
2023-09-17 20:25:59 +0300

    add a file to manange all the prints which will be switched by GUI

---
+++ sw/user_interface.py
@@ -0,0 +1,54 @@
+#!/opt/homebrew/bin/python3
+
+import sys
+import os
+import subprocess
+import tkinter
+import logic_config
+import connection_module as connect
+import defines
+import data_mgm
+import binascii
+
+"""
+file : maim.py
+owner : shaharf
+description : main file to call each class and excute commands and functionality.
+comments: # todo shahar later will switch to GUI
+"""
+
+def print_hello_msg():
+    print("########################################")
+    print("Wellcome to phase measurment application")
+    print("########################################")
+    print("step 1: setting up connection to red pitya.")
+    print("please retrive the the ip address and the port your red pitya is listening")
+
+def get_ip_addr():
+    ip_addr = input("Enter IP address of Red Pitaya\n")
+    return ip_addr
+
+def get_port():
+    port = input("Enter Port Num of Red Pitaya\n")
+    return port
+
+def print_config_setup_msg():
+    print("step 2: please retrive the freq of the oscillator you want to check")
+
+def get_freq_from_user():
+    freq = input("Enter Freq of the external oscillator\n")
+    return freq
+
+def print_wait_for_start_op():
+    print("set up complete")
+    print("please connect the oscillator to the red pitya and when reday follow the instruction on the screen")
+
+def get_from_user_start_op_indication():
+    result = input("please hit 1 when ready\n")
+    return result
+
+def print_wait_to_finish_op():
+    print("please wait for finish operation")
+
+def print_data_unload():
+    print("red pitya finished its operation, start unloading data")
