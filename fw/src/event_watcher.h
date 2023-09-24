c47e4df7738e72a5e34d295721224f55c646f348
shaharfeingold <110739184+shaharfeingold@users.noreply.github.com>
2023-09-24 22:24:25 +0300

    fix comp6

--- fw/src/event_watcher.h
+++ fw/src/event_watcher.h
@@ -16,9 +16,9 @@ typedef struct _ThreadArgs{
 #define EVENT_OCCUER SIGUSR1
 #define CLIENT_WANTS_TO_CLOSE SIGUSR2
 
-void* init_thread_args_struct(ThreadArgs* thread_args, LogicConfig* logic_config, pid_t parent_pid, int* client_socket_ptr, Events* events);
+void init_thread_args_struct(ThreadArgs* thread_args, LogicConfig* logic_config, pid_t parent_pid, int* client_socket_ptr, Events* events);
 
-void mainEventThread(void* args);
+void* mainEventThread(void* args);
 void monitorEvents(Events* event_struct);
 
 void events_signals_handler(int sig);
