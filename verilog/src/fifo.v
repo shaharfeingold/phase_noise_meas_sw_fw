d043a41a5c285ce149b594bae3c24dbdbdecc1eb
shaharfeingold <110739184+shaharfeingold@users.noreply.github.com>
2023-09-30 09:47:48 +0300

    add feature flush + add to test bench overflow simulation

--- verilog/src/fifo.v
+++ verilog/src/fifo.v
@@ -2,7 +2,7 @@
 
 
 module fifo(clk, rstn, in_data, in_data_vld, out_data_rdy, out_data_vld, 
-            out_data, out_data_ptr, fifo_size, event_overflow, event_underrun);
+            out_data, out_data_ptr, fifo_size, flush_fifo, event_overflow, event_underrun);
 //parameters
 parameter DATA_WIDTH = 32; 
 parameter FIFO_SIZE = 1024;
@@ -18,34 +18,41 @@ input out_data_vld;
 output [FIFO_SIZE_WIDTH-1 : 0] out_data_ptr;
 output out_data_rdy;
 
-output reg [FIFO_SIZE_WIDTH-1 : 0] fifo_size;
+output reg [FIFO_SIZE_WIDTH : 0] fifo_size;
 output event_overflow;
 output event_underrun; //event set to 1 when trying to pop from empty fifo
+
+input flush_fifo;
 
 //implementaion
 reg [DATA_WIDTH-1 : 0] data [0 : FIFO_SIZE]; //stores the data in fifo //todo shahar later switch the implement using ff to ease code + ease debug
 reg [FIFO_SIZE_WIDTH-1 : 0] current_data_ptr;
 wire [FIFO_SIZE_WIDTH-1 : 0] current_data_ptr_ns;
-wire [FIFO_SIZE_WIDTH-1 : 0] fifo_size_ns;
+wire [FIFO_SIZE_WIDTH : 0] fifo_size_ns;
 reg [FIFO_SIZE_WIDTH-1 : 0] fifo_head_ptr;
 wire [FIFO_SIZE_WIDTH-1 : 0] fifo_head_ptr_ns;
+reg [FIFO_SIZE_WIDTH-1 : 0] fifo_data_out_ptr;
+wire [FIFO_SIZE_WIDTH-1 : 0] fifo_data_out_ptr_ns;
+wire internal_reset = rstn & (~flush_fifo);
 
 integer i;
 always @(posedge clk) begin
-    if (~rstn) begin
+    // if (~rstn) begin
+    if (~internal_reset) begin
         for(i=0 ; i < FIFO_SIZE; i = i+1) begin
             data[i] <= {DATA_WIDTH{1'b0}};
         end 
         current_data_ptr <= {FIFO_SIZE_WIDTH{1'b0}};
         fifo_size <= {FIFO_SIZE_WIDTH{1'b0}};
         fifo_head_ptr <= {FIFO_SIZE_WIDTH{1'b0}};
+        fifo_data_out_ptr <= {FIFO_SIZE_WIDTH{1'b0}};
     end
     else begin
         //push to fifo
         data[current_data_ptr] <= in_data_vld ? in_data : data[current_data_ptr];
-        current_data_ptr <= current_data_ptr_ns == (FIFO_SIZE-1) ? {FIFO_SIZE_WIDTH{1'b0}} : current_data_ptr_ns;  //for cyclic implementaion
+        current_data_ptr <= current_data_ptr_ns == (FIFO_SIZE) ? {FIFO_SIZE_WIDTH{1'b0}} : current_data_ptr_ns;  //for cyclic implementaion
         //pop from fifo
-        fifo_head_ptr <= fifo_head_ptr == (FIFO_SIZE-1) ? {FIFO_SIZE_WIDTH{1'b0}} : fifo_head_ptr_ns;
+        fifo_head_ptr <= fifo_head_ptr == (FIFO_SIZE) ? {FIFO_SIZE_WIDTH{1'b0}} : fifo_head_ptr_ns;
         //misc
         fifo_size <= fifo_size_ns;
     end
@@ -59,10 +66,11 @@ assign fifo_head_ptr_ns = out_data_vld & out_data_rdy ? fifo_head_ptr +  {{FIFO_
 //misc
 assign fifo_size_ns = in_data_vld & out_data_vld & out_data_rdy ? fifo_size :
                       in_data_vld ? fifo_size + {{FIFO_SIZE_WIDTH-1{1'b0}},1'b1} : 
-                      out_data_vld & out_data_rdy ? fifo_size - {{FIFO_SIZE_WIDTH-1{1'b0}},1'b1} :
+                      out_data_vld & out_data_rdy ? fifo_size - {{FIFO_SIZE_WIDTH{1'b0}},1'b1} :
                       fifo_size;
 assign out_data = data[fifo_head_ptr];
 assign out_data_ptr = fifo_head_ptr;
 assign out_data_rdy = (fifo_size > 0);
 assign event_underrun = out_data_vld & ~(|fifo_size);
+//todo shahar review fifo size width  - I add extra bit to maintain the fifo size itself and not - 1
 endmodule
--- verilog/src/fifo_tb.v
+++ verilog/src/fifo_tb.v
@@ -3,7 +3,7 @@ module fifo_tb();
 
 //parameter
 parameter DATA_WIDTH = 32; 
-parameter FIFO_SIZE = 1024;
+parameter FIFO_SIZE = 5;
 localparam FIFO_SIZE_WIDTH = $clog2(FIFO_SIZE);
 
 //regs - stub
@@ -11,16 +11,18 @@ reg clk, rstn;
 reg [DATA_WIDTH-1 : 0] in_data;
 reg in_data_vld;
 reg out_data_vld;
+reg flush_fifo;
 
 //wire - output
 wire out_data_rdy;
 wire [DATA_WIDTH-1 : 0] out_data;
 wire [FIFO_SIZE_WIDTH-1 : 0] out_data_ptr;
-wire [FIFO_SIZE_WIDTH-1 : 0] fifo_size;
+wire [FIFO_SIZE_WIDTH : 0] fifo_size;
 wire event_overflow;
+wire event_underrun;
 
 //modules
-fifo fifo_0(
+fifo #(.FIFO_SIZE(5)) fifo_0(
     .clk(clk), 
     .rstn(rstn), 
     .in_data(in_data), 
@@ -30,7 +32,9 @@ fifo fifo_0(
     .out_data(out_data), 
     .out_data_ptr(out_data_ptr), 
     .fifo_size(fifo_size), 
-    .event_overflow(event_overflow)
+    .flush_fifo(flush_fifo),
+    .event_overflow(event_overflow),
+    .event_underrun(event_underrun)
     );
 
 //simulation
@@ -44,6 +48,7 @@ initial begin
     rstn = 1'b1;
     in_data_vld = 1'b0;
     out_data_vld = 1'b0;
+    flush_fifo = 1'b0;
     $display("[%0t] start reset dut", $time);
     #1 
     rstn = 1'b0;
@@ -54,12 +59,63 @@ initial begin
     #50;
     $display("[%0t] finish relax", $time);
     $display("[%0t] start inject data", $time);
+    $display("[%0t] write the value 32'h00000001", $time);
     in_data_vld = 1'b1;
     in_data = 32'h00000001;
     #10;
+    $display("[%0t] fifo size = %d", $time ,fifo_size);
+    $display("[%0t] write the value 32'h00000002", $time);
     in_data = 32'h00000002;
     #10;
+    $display("[%0t] fifo size = %d", $time ,fifo_size);
     in_data_vld = 1'b0;
+    #10;
+    //read and write at the same clock
+    $display("[%0t] write the value 32'h00000003", $time);
+    $display("[%0t] issue a read from the fifo", $time);
+    out_data_vld = 1'b1;
+    in_data_vld = 1'b1;
+    in_data = 32'h00000003;
+    #10;
+    $display("[%0t] fifo size = %d", $time ,fifo_size);
+    $display("[%0t] issue a read from the fifo", $time);
+    in_data_vld = 1'b0;
+    out_data_vld = 1'b1;
+    #10;
+    $display("[%0t] fifo size = %d", $time ,fifo_size);
+    out_data_vld = 1'b0;
+    //sim over flow 
+    $display("[%0t] going to inject overflow", $time);
+    #10;
+    $display("[%0t] write the value 32'h00000004", $time);
+    in_data_vld = 1'b1;
+    in_data = 32'h00000004;
+    #10;
+    $display("[%0t] fifo size = %d", $time ,fifo_size);
+    $display("[%0t] write the value 32'h00000005", $time);
+    in_data = 32'h00000005;
+    #10; 
+    $display("[%0t] fifo size = %d", $time ,fifo_size);
+    $display("[%0t] write the value 32'h00000006", $time);
+    in_data = 32'h00000006;
+    #10; 
+    $display("[%0t] fifo size = %d", $time ,fifo_size);
+    $display("[%0t] write the value 32'h00000007", $time);
+    in_data = 32'h00000007;
+    #10;
+    $display("[%0t] fifo size = %d", $time ,fifo_size);
+    $display("[%0t] write the value 32'h00000008", $time);
+    in_data = 32'h00000008;
+    #10;
+    $display("[%0t] fifo size = %d", $time ,fifo_size);
+    $display("[%0t] overflow ? %d", $time, event_overflow);
+    in_data_vld = 1'b0;
+    #50;
+    //flush fifo
+    $display("[%0t] flushing fifo", $time);
+    flush_fifo = 1'b1;
+    #10;
+    flush_fifo = 1'b0;
     #50;
     $display("[%0t] end simulation", $time);
     $finish;
