0fcf8eeb68232618f786e6fb550d0e040364f2c7
Shahar Feingold <feingold1@mail.tau.ac.il>
2023-12-30 10:51:56 +0000

    fix methods converting fixed point to float

--- fw/src/utils_function.c
+++ fw/src/utils_function.c
@@ -90,7 +90,8 @@ float convert_fix_point_to_float(uint32_t fixed_point){
     verb_print(HIGH, "enterd convert_fix_point_to_float\n");
     float result = 0;
    // uint32_t sign_extended = fixed_point << (32 - ARRAY_NUM_WIDTH); //todo review no need to sign extend becuase we are already in 32 bit width num
-    result = (float)fixed_point / (uint32_t)(1 << (ARRAY_FRCTIONAL_BIT + (32 - ARRAY_NUM_WIDTH)));
+	int cast = (int) fixed_point;
+    result = (float)cast / (uint32_t)(1 << (ARRAY_FRCTIONAL_BIT + (32 - ARRAY_NUM_WIDTH)));
     verb_print(HIGH, "result from convert_fix_point_to_float : %f\n", result);
     return result;
 }
